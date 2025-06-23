d3e7157265d1e2b7c9a20d495c7bd1766a1695c8
MayankRathore194 <mayankrathore@gmail.com>
2025-06-23 18:01:03 +0530

    Assignment 1 question 1 edit

--- Assignments/Assignment 1/Resultprogram.py
+++ Assignments/Assignment 1/Resultprogram.py
@@ -11,13 +11,4 @@ p=(t/500)*100
 a=p
 print("Name of the student:",name)
 print("Total marks scored:",t)
-if a<=40:
-    print("fail")
-elif a>40 and a<=60:
-    print("grade is c")
-elif a>60 and a<=90:
-    print("grade is b")
-else:
-    print("grade a")
-
 print("Percentage:",p)
